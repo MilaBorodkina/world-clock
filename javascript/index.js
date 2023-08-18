f943d31f76da09ad268f4c526a3da2c453e53259
Mila <142541687+MilaBorodkina@users.noreply.github.com>
2023-08-18 12:35:18 +0200

    world clock project

---
+++ javascript/index.js
@@ -0,0 +1,57 @@
+function updateTime() {
+  //New York
+  let NYElement = document.querySelector("#new-york-city");
+  if (NYElement) {
+    let NYDateElement = NYElement.querySelector(".date");
+    let NYTimeElement = NYElement.querySelector(".time");
+    let NYTime = moment().tz("America/New_York");
+
+    NYDateElement.innerHTML = NYTime.format("DD MMMM YYYY");
+    NYTimeElement.innerHTML = NYTime.format("HH:mm:ss");
+  }
+
+  //London
+  let londonElement = document.querySelector("#london");
+  if (londonElement) {
+    let londonDateElement = londonElement.querySelector(".date");
+    let londonTimeElement = londonElement.querySelector(".time");
+    let londonTime = moment().tz("Europe/London");
+
+    londonDateElement.innerHTML = londonTime.format("DD MMMM YYYY");
+    londonTimeElement.innerHTML = londonTime.format("HH:mm:ss");
+  }
+
+  //Singapore
+  let SingaporeElement = document.querySelector("#singapore");
+  if (SingaporeElement) {
+    let SingaporeDateElement = SingaporeElement.querySelector(".date");
+    let SingaporeTimeElement = SingaporeElement.querySelector(".time");
+    let SingaporeTime = moment().tz("Asia/Singapore");
+
+    SingaporeDateElement.innerHTML = SingaporeTime.format("DD MMMM YYYY");
+    SingaporeTimeElement.innerHTML = SingaporeTime.format("HH:mm:ss");
+  }
+}
+setInterval(updateTime, 1000);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("DD MMMM YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format("HH:mm:ss")}</div>
+        </div>
+        <a href="/">All cities</a>`;
+}
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
